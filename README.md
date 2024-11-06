beaeec5ad76364b9fdded3657a2c99ea158e4d77
viktorhugo <viktor1103@gmail.com>
2024-11-06 00:15:15 -0500

    added funct. deposit Collateral

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 1. (Relative Stability) Anchored or Pegged  -> $1.00
 <!-- Como podemos asegurarnos que este siempre vinculado a un dolar -->
-    1. Chainlink proce feed
-    2. 
+    a. Chainlink process feed
+    b. 
 2. Stability Mechanism (Minting): Algorithmic (Decentralized). 
 <!-- No habra ninguna entidad centralizada que  vaya a acuñar, burn o mantener el precio,-->
 <!-- No existe una entidad controladora que controle nuestra stable coin -->
 <!-- una mejor moneda para la web3 probablemente sea anclada o vinculada -->
-    1. People can only mint the stablecoin with enough collateral (coded)
+    a. People can only mint the stablecoin with enough collateral (coded)
 
 3. Collateral: Exogenus (Crypto => wETH, wBTC ).
--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -23,6 +23,11 @@
 
 pragma solidity ^0.8.25;
 
+import { console } from "forge-std/Script.sol";
+import { DecentralizedStableCoin } from "./DecentralizedStableCoin.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol"; 
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 /**
  * @title DSCEngine
  * @author Victor mosquera
@@ -39,40 +44,40 @@ pragma solidity ^0.8.25;
  * @notice this contract is VERY loosely based on the MakerDAO (DAI) system.
  */
 
-import { console } from "forge-std/Script.sol";
-
-contract DSCEngine {
+contract DSCEngine is ReentrancyGuard{
 
     ///////////////////
     //* Errors    //
     ///////////////////
     error DSCEngine__MustBeMoreThanZero();
     error DSCEngine__TokenAddresessAndPriceFeedAdressessMustBeSameLenght();
-
+    error DSCEngine__NotAllowedToken();
+    error DSCEngine__TransferTokenCollateralFailed();
 
     ///////////////////////
     //* State Variables  //
     //////////////////////
     mapping(address token => address priceFeed) private s_priceFeeds; // token price feed
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+    DecentralizedStableCoin private immutable i_dsc; // DSC contract
 
+    ///////////////////////
+    //*    Events       //
+    //////////////////////
+    event CollateralDeposited(address indexed user, address indexed token, uint256 amount);
 
     ///////////////////
     //* Modifiers    //
     ///////////////////
     modifier moreThanZero(uint256 amount) {
-        if (amount <= 0) {
-            revert DSCEngine__MustBeMoreThanZero();
-        }
+        if (amount <= 0) revert DSCEngine__MustBeMoreThanZero();
         _;
     }
 
-    // modifier isAllowToken(address addressToken) { // check if token is allowed
-    //     if (amount <= 0) {
-    //         revert DSCEngine__MustBeMoreThanZero();
-    //     }
-    //     _;
-    // }
-
+    modifier isAllowToken(address addressToken) { // check if token is allowed
+        if (s_priceFeeds[addressToken]  == address(0)) revert DSCEngine__NotAllowedToken();
+        _;
+    }
 
      ///////////////////
     //* Functions    //
@@ -80,18 +85,20 @@ contract DSCEngine {
     constructor(
         address[] memory tokenAddressess,
         address[] memory priceFeedAddressess,
-        address dscAddress
+        address dscAddress // direccion de la moneda estable descentralizada
     ) {
         // USD price feeds
         if (tokenAddressess.length != priceFeedAddressess.length) {
             revert DSCEngine__TokenAddresessAndPriceFeedAdressessMustBeSameLenght();
         }
         // For example ETH / USD BTC / USD,MKR / USD, etc.. vamos a recorrer la matriz de direcciones de token
+        // Los tokens que estan permitidos en la plataforma entonces si tienen un feed de precios, estan permitidos
         for (uint256 i = 0; i < tokenAddressess.length; i++) {
+            // llenamos el mapping
             s_priceFeeds[tokenAddressess[i]] = priceFeedAddressess[i];
         }
+        i_dsc = DecentralizedStableCoin(dscAddress);
     }
-
 
      /////////////////////////
     //* External Functions //
@@ -99,14 +106,29 @@ contract DSCEngine {
 
      // elegir que tipo de garantia quiere depositar 
     /*
+    * @notice follows CEI pattern (verifica las interacciones de los efectos)
     * @param tokenCollateralAdrress the address of the collateral deposit token
     * @param amountCollateral the amount of collateral deposit
     */
     function depositCollateral(
         address tokenCollateralAdrress, 
         uint256 amountCollateral
-    ) moreThanZero(amountCollateral) external {
+    ) 
+        external 
+        moreThanZero(amountCollateral) 
+        isAllowToken(tokenCollateralAdrress) // check if tokenCollateralAdrress is allowed
+        nonReentrant { // nonReentrant verification (ataques mas comunes en la web3) 
+            // es buena practica cuando se ejecuctan contratos externos, ppuede que consuma un poco mas de gas pero es mas seguro
 
+        // 1. hacer una manera de rastrear cuanta garantia alguien ha depositado
+        s_collateralDeposited[msg.sender][tokenCollateralAdrress] += amountCollateral;
+        // actualizando el estado emitimos un evento
+        emit CollateralDeposited(msg.sender, tokenCollateralAdrress, amountCollateral);
+        // 2. ahora conseguir los tokens, vamos a nesecitar un wrap al collateral como un ERC20 
+        bool success = ERC20(tokenCollateralAdrress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) {
+            revert DSCEngine__TransferTokenCollateralFailed();
+        }
     }
 
     function depositCollateralAndMintDsc() external {}
@@ -123,6 +145,5 @@ contract DSCEngine {
 
     // permite ver que tn saludables estan las personas
     function getHealthFactor() external view returns (uint256) {}
-
 
 }
